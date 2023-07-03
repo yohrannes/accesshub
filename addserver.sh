5e18f70afa71bf263de60fa3749d0e7800cb2e81
yohannes <yohrannes@gmail.com>
2023-07-03 10:31:55 +0000

    added new variables for filter information and organize in regions

--- addserver.sh
+++ addserver.sh
@@ -2,6 +2,7 @@
 source ./.sourcedata/.accessdata
 source ./.sourcedata/.userdata
 source ./.sourcedata/.userpassword
+source ./.sourcedata/.hostaddress
 
 function newserver {
 while true; do
@@ -12,6 +13,8 @@ while true; do
     sidtotal=0
     uidtotal=0
     pidtotal=0
+    hostidtotal=0
+    portidtotal=0
 
     indexcount=0
 
@@ -21,14 +24,22 @@ while true; do
         then
             sidtotal=$((sidtotal+1))
         fi
+
         if [[ $indexcount == uid* ]]
         then
             uidtotal=$((uidtotal+1))
         fi
-        if [[ $indexcount == uid* ]]
+
+        if [[ $indexcount == pid* ]]
         then
-            uidtotal=$((pidtotal+1))
+            pidtotal=$((pidtotal+1))
         fi
+
+        if [[ $indexcount == hostid* ]]
+        then
+            hostidtotal=$((hostidtotal+1))
+        fi
+
     done
 
     # Find index out of order
@@ -64,6 +75,17 @@ while true; do
             break
         fi
         orderpid=0
+    done
+
+    for ((i=0; i<=hostidtotal; i++))
+    do
+        indexhostid="hostid$i"
+        if [[ ! ${DATA[$indexhostid]} ]]
+        then
+            orderhostid=1
+            break
+        fi
+        orderhostid=0
     done
 
     # Find the value on the next disponible index
@@ -113,22 +135,47 @@ while true; do
         pidtotal=$((pidtotal+1))
     fi
 
-    read -r -p "Name :" idsid
-    read -r -p "Server User :" iduid
-    read -r -p "Password :" idpid
-    read -p "Host: " idipid
-    read -p "Port: " idpt
-    read -p "Server type-group: " idtp
-    read -p "Server region-group: " idrg
-    read -p "Server sub-region group: " idsbrg
+    if [[ $orderhostid == 1 ]]
+    then
+        for ((i=0; i<=hostidtotal+1; i++))
+        do
+            indexhostid="hostid$i"
+            if [[ ! ${DATA[$indexhostid]} ]]
+            then
+                hostidtotal=$i
+                break
+            fi
+        done
+    else
+        hostidtotal=$((hostidtotal+1))
+    fi
+
+    read -r -p "Name :" sid
+    read -r -p "Server User :" uid
+    read -r -p "Password :" pid
+    read -r -p "Host: " hostid
+    read -r -p "Specify port? (if not default 22) [y/n]" port
+    case $port in
+    y)
+        read -r -p "Port: " portid
+    ;;
+    esac
+    read -r -p "Server type-group [infra,app,backup, etc.]: " typeid
+    read -r -p "Server region-group [country]: " regionid
+    read -r -p "Server sub-region group [state]: " sbregionid
 
     # Add the id value array on the next disponible index
-    DATA["sid$sidtotal"]=$idsid
-    DATA["uid$uidtotal"]=$iduid
-    DATA["pid$pidtotal"]=$idpid
-    echo "DATA[sid$sidtotal]='$idsid'" >> ./.sourcedata/.accessdata
-    echo "DATA[uid$uidtotal]='$iduid'" >> ./.sourcedata/.userdata
-    echo "DATA[pid$pidtotal]='$idpid'" >> ./.sourcedata/.userpassword
+    DATA["sid$typeid$regionid$sbregionid$sidtotal"]=$sid
+    DATA["uid$typeid$regionid$sbregionid$uidtotal"]=$uid
+    DATA["pid$typeid$regionid$sbregionid$pidtotal"]=$pid
+    DATA["hostid$typeid$regionid$sbregionid$hostidtotal"]=$hostid
+    DATA["portid$typeid$regionid$sbregionid$portidtotal"]=$portid
+  
+    echo "DATA[sid$typeid$regionid$sbregionid$sidtotal]='$sid'" >> ./.sourcedata/.accessdata
+    echo "DATA[uid$typeid$regionid$sbregionid$uidtotal]='$uid'" >> ./.sourcedata/.userdata
+    echo "DATA[pid$typeid$regionid$sbregionid$pidtotal]='$pid'" >> ./.sourcedata/.userpassword
+    echo "DATA[hostid$typeid$regionid$sbregionid$hostidtotal]='$hostid'" >> ./.sourcedata/.hostaddress
+    echo "DATA[portid$typeid$regionid$sbregionid$portidtotal]='$portid'" >> ./.sourcedata/.hostports
 
     read -r -p "Would you like to add more data? [y/n]: " MOREDATA
 
